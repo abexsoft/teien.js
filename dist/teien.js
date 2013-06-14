1d6bcbfebde7ed2d610cce51b107932af31cbc28
abexsoft <abexsoft@gmail.com>
2013-06-15 02:11:34 +0900

    fixed a bug.

--- dist/teien.js
+++ dist/teien.js
@@ -452,7 +452,8 @@ if (typeof exports === 'undefined'){
 	 var app = require('http').createServer(handler);
 	 var io = require('socket.io').listen(app, {'log level': 1});
 					      
-	 app.listen(8080);
+//	 app.listen(8080);
+	 app.listen(80);
 	 
 	 io.sockets.on('connection', 
 		       function (socket) {
